edc00b7b22fd06e913dce343871f5f775769ae05
vocongthanh247 <vocongthanh21022000@gmail.com>
2024-12-01 22:00:14 +0700

    Update add

--- src/test/java/BrowserStackTest.java
+++ src/test/java/BrowserStackTest.java
@@ -7,13 +7,13 @@ import java.net.URL;
 public class BrowserStackTest {
     public static void main(String[] args) {
         String userName = "thanhvo_DtH7RP"; // Thay bằng tên đăng nhập BrowserStack của bạn
-        String accessKey = "thanhvo_DtH7RP"; // Thay bằng Access Key của bạn
+        String accessKey = "AH576ScJ6B8qgD1DxLz6"; // Thay bằng Access Key của bạn
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("device", "Samsung Galaxy A32");
-        caps.setCapability("os_version", "11.0"); // Cập nhật phiên bản nếu cần
+        caps.setCapability("device", "Samsung Galaxy S21"); // Thay đổi thiết bị
+        caps.setCapability("os_version", "12.0"); // Cập nhật phiên bản OS
         caps.setCapability("browserstack.local", "false");
-        caps.setCapability("name", "Samsung Galaxy A32 Test");
+        caps.setCapability("name", "Samsung Galaxy S21 Test");
 
         try {
             WebDriver driver = new RemoteWebDriver(
